c08a5eb54d079784113a514698f33d3e01c12bb5
IceSentry <c.giguere42@gmail.com>
2024-12-04 23:53:24 -0500

    clean up day03

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -13,100 +13,86 @@ fn parse(input: &str) -> &str {
     input
 }
 
-fn match_str(chars: &mut Chars, str: &str) -> bool {
-    for c in str.chars() {
-        if chars.next().unwrap() != c {
+// tries to match a str, if the string matches advance the iterator by its len. Otherwise the
+// iterator will not be advanced
+fn peek_match(chars: &mut Chars, search: &str) -> bool {
+    let mut peekable = chars.clone().peekable();
+    for c in search.chars() {
+        let Some(peek) = peekable.peek() else {
+            return false;
+        };
+        if *peek == c {
+            peekable.next();
+        } else {
             return false;
         }
     }
+    for _ in 0..search.len() {
+        chars.next();
+    }
     true
+}
+
+fn get_number(chars: &mut Chars, end_char: char) -> Option<i32> {
+    let mut number = 0;
+    for c in chars.by_ref() {
+        if c.is_ascii_digit() {
+            number *= 10;
+            number += c.to_digit(10).unwrap();
+        } else if c == end_char {
+            break;
+        } else {
+            return None;
+        }
+    }
+    Some(number as i32)
 }
 
 fn part_1(input: &str) -> i32 {
     let mut total = 0;
     let mut chars = input.chars();
-    'next: while let Some(c) = chars.next() {
-        if c == 'm' && match_str(&mut chars, "ul(") {
+    'next: loop {
+        if peek_match(&mut chars, "mul(") {
             //println!("mul( found");
-            let mut left_number = 0;
-            let mut right_number = 0;
-            for c in chars.by_ref() {
-                if c.is_ascii_digit() {
-                    left_number *= 10;
-                    left_number += c.to_digit(10).unwrap();
-                    continue;
-                } else if c == ',' {
-                    break;
-                }
+            let Some(left_number) = get_number(&mut chars, ',') else {
                 continue 'next;
-            }
-            for c in chars.by_ref() {
-                if c.is_ascii_digit() {
-                    right_number *= 10;
-                    right_number += c.to_digit(10).unwrap();
-                    continue;
-                } else if c == ')' {
-                    break;
-                }
+            };
+            let Some(right_number) = get_number(&mut chars, ')') else {
                 continue 'next;
-            }
+            };
             //println!("{left_number} x {right_number}");
             total += left_number * right_number;
+        } else if chars.next().is_none() {
+            break;
         }
     }
-    total as i32
+    total
 }
 
 fn part_2(input: &str) -> i32 {
     let mut total = 0;
     let mut chars = input.chars();
     let mut enabled = true;
-    'next: while let Some(c) = chars.next() {
-        if c == 'm' && match_str(&mut chars, "ul(") {
-            //println!("mul( found");
-            let mut left_number = 0;
-            let mut right_number = 0;
-            for c in chars.by_ref() {
-                if c.is_ascii_digit() {
-                    left_number *= 10;
-                    left_number += c.to_digit(10).unwrap();
-                    continue;
-                } else if c == ',' {
-                    break;
-                }
+    'next: loop {
+        if peek_match(&mut chars, "mul(") {
+            let Some(left_number) = get_number(&mut chars, ',') else {
                 continue 'next;
-            }
-            for c in chars.by_ref() {
-                if c.is_ascii_digit() {
-                    right_number *= 10;
-                    right_number += c.to_digit(10).unwrap();
-                    continue;
-                } else if c == ')' {
-                    break;
-                }
+            };
+            let Some(right_number) = get_number(&mut chars, ')') else {
                 continue 'next;
-            }
-            //println!("{left_number} x {right_number}");
+            };
             if enabled {
                 total += left_number * right_number;
             }
-        } else if c == 'd' {
-            if let Some('o') = chars.next() {
-                if let Some(c) = chars.next() {
-                    if c == '(' {
-                        if let Some(')') = chars.next() {
-                            // enabled
-                            enabled = true;
-                        }
-                    } else if c == 'n' && match_str(&mut chars, "'t()") {
-                        // disabled
-                        enabled = false;
-                    }
-                }
-            }
+        } else if peek_match(&mut chars, "do()") {
+            enabled = true;
+        } else if peek_match(&mut chars, "don't()") {
+            enabled = false;
+        } else if chars.next().is_none() {
+            break;
         }
     }
-    total as i32
+    total
 }
 
 #[cfg(test)]
